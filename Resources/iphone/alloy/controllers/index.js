be6dab69ca7169bc326f840c6ee0cf9dbf41338e
Ahmed Atif <ahmed.atif15@gmail.com>
2014-07-07 15:49:57 +0200

    Integrating FTR
    After compiling

---
+++ Resources/iphone/alloy/controllers/index.js
@@ -0,0 +1,19 @@
+function Controller() {
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__controllerPath = "index";
+    arguments[0] ? arguments[0]["__parentSymbol"] : null;
+    arguments[0] ? arguments[0]["$model"] : null;
+    arguments[0] ? arguments[0]["__itemTemplate"] : null;
+    var $ = this;
+    var exports = {};
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    var appOrFTRWin;
+    var prefixWin = "";
+    appOrFTRWin = firstTime ? Alloy.createController(prefixWin + "FTR/index").getView() : Alloy.createController(prefixWin + "appTabGroup").getView();
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._;
+
+module.exports = Controller;
